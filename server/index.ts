0a03c39f8203708f568c9fcf0b702a7832d867f4
coodeex <juhani.s.lehto@gmail.com>
2022-12-19 00:33:10 +0200

    run eslint

--- server/index.ts
+++ server/index.ts
@@ -1,33 +1,34 @@
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
-import express from "express";
-import { sendMessage } from "./services/telegram";
-import { blueLog, redLog } from "./services/util/colorLog";
-import { sleep } from "./services/util/sleep";
+import express from 'express';
+import { sendMessage } from './services/telegram';
+import { blueLog, redLog } from './services/util/colorLog';
+import { sleep } from './services/util/sleep';
 
 const app = express();
-app.use(express.json());  
+app.use(express.json());
 
-
-app.get("/", (req, res) => {
-  res.send("Hello ts!");
+app.get('/', (req, res) => {
+  res.send('Hello ts!');
 });
-app.get("/r", async (req, res) => {
+app.get('/r', async (req, res) => {
   await sleep(2000);
-  res.send("Hello r!");
+  res.send('Hello r!');
 });
 
-app.post("/tg", async (req, res) => {
+app.post('/tg', async (req, res) => {
   const body = req.body;
   const msg = body?.message;
-  try{
+  try {
     await sendMessage(msg);
-  }catch{
-    blueLog("catched");
+  } catch {
+    blueLog('catched');
     res.status(400);
   }
-  res.json("sent");
+  res.json('sent');
 });
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server is up and running at http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is up and running at http://localhost:${PORT}`),
+);
--- server/services/telegram.ts
+++ server/services/telegram.ts
@@ -1,19 +1,18 @@
-import TelegramBot from "node-telegram-bot-api";
-import { error, errors } from "./error";
-import { blueLog, redLog } from "./util/colorLog";
+import TelegramBot from 'node-telegram-bot-api';
+import { error, errors } from './error';
+import { blueLog, redLog } from './util/colorLog';
 
-const TOKEN = process.env.TG_API_KEY || "";
-const chatId = process.env.TG_CHANNEL_ID || "";
+const TOKEN = process.env.TG_API_KEY || '';
+const chatId = process.env.TG_CHANNEL_ID || '';
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-
-export const sendMessage = async (message: string|undefined) => {
-  if(!message){
+export const sendMessage = async (message: string | undefined) => {
+  if (!message) {
     error(errors.noMessage);
     return;
   }
 
   await bot.sendMessage(chatId, message);
-  console.log("message sent");
+  console.log('message sent');
 };
