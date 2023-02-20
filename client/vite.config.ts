eb29ffbde1978fa92fe76ca88e998a9fa566e517
coodeex <juhani.s.lehto@gmail.com>
2023-02-20 16:05:03 +0200

    use static web app

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,13 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     hmr: {
-      clientPort: 3284,
+      clientPort: parseInt(process.env.PORT) || 3284,
     },
     watch: {
       usePolling: true,
     },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
-    port: 3284,
+    port: parseInt(process.env.PORT) || 3284,
   },
 });
